fe6f018c27c909dc46abdd0363ee110efa29aaf3
glawing42 <graham.lawing@gmail.com>
2017-08-22 00:06:06 -0400

    added breaks on output

--- liri.js
+++ liri.js
@@ -23,8 +23,8 @@ if(liriArg === "my-tweets"){
             
             var tweets = data;
             for(var i = 0; i<tweets.length; i++){
-                console.log(tweets[i].text);
-                console.log(tweets[i].created_at);
+                console.log(tweets[i].text + "\r\n");
+                console.log(tweets[i].created_at + "\r\n");
             }
         };
 }
@@ -44,12 +44,12 @@ if(liriArg === "spotify-this-song"){
             var data = data.tracks.items;
             for(var i =0; i < data.length; i++){
                 
-                console.log("Song: " + data[i].name); //song track name
-                console.log("Album: " + data[i].album.name); //album name
-                console.log("Preview Link: " + data[i].preview_url); //preview link to the song
+                console.log("Song: " + data[i].name + "\r\n"); //song track name
+                console.log("Album: " + data[i].album.name + "\r\n"); //album name
+                console.log("Preview Link: " + data[i].preview_url + "\r\n"); //preview link to the song
             
                 for(var j =0; j < data[i].artists.length; j++){
-                    console.log("Artist: " + data[i].artists[j].name); //artist's name
+                    console.log("Artist: " + data[i].artists[j].name + "\r\n"); //artist's name
                 }
             }
         }else{
@@ -74,14 +74,14 @@ if(liriArg === "movie-this"){
     request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&r=json&tomatoes=true&apikey=40e9cece",function (error, response, body){
         
         if(process.argv[3]){
-        console.log("Movie Title: " + JSON.parse(body).Title);  
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-        console.log("Country: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Movie Title: " + JSON.parse(body).Title + "\r\n");  
+        console.log("Release Year: " + JSON.parse(body).Year + "\r\n");
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n");
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\r\n");
+        console.log("Country: " + JSON.parse(body).Country + "\r\n");
+        console.log("Language: " + JSON.parse(body).Language + "\r\n");
+        console.log("Plot: " + JSON.parse(body).Plot + "\r\n");
+        console.log("Actors: " + JSON.parse(body).Actors + "\r\n");
        
         }else{
             request("http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&r=json&tomatoes=true&apikey=40e9cece",function(error, response,body){
